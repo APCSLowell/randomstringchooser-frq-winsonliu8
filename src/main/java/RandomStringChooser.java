3b2bda6a4e31626ab8e705e64ef3b3f6938fd7d3
winsonliu8 <146293819+winsonliu8@users.noreply.github.com>
2025-04-26 12:04:17 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -6,7 +6,7 @@ public class RandomStringChooser
  public RandomStringChooser(String[] wordArray)
  {
  words = new ArrayList<String>();
- for (int i = 0; i < wordArray.size(); i++)
+ for (int i = 0; i < wordArray.length; i++)
   //for (String w: wordArray)
   //words.add(w); 
    words.add(i);
